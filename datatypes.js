7cd652ef907dd445d2dc1d8cb00e6e2c85268ecd
PIYUSH-IIITL <piyushnayak90020@gmail.com>
2024-01-01 14:54:53 +0530

    learn some basics

---
+++ datatypes.js
@@ -0,0 +1,12 @@
+let pName="piyush"
+let age= 20
+let genderMale = true
+
+let crush=null
+let goal;
+
+console.log(typeof pName)
+console.log(typeof age)
+console.log(typeof genderMale )
+console.log(typeof crush)
+console.log(typeof goal)
---
+++ test1.js
@@ -0,0 +1,17 @@
+const accId= 123456
+let pName ="piyush"
+var lName = "nayak";
+let mNAME;
+
+
+ mNo = "hii"
+pName ="rishu" 
+lName = "gupta"
+// for executing many var together with one console
+console.table([ pName, lName,mNo])
+console.log(pName)
+console.log(lName)
+console.log(mNo)
+console.log("pName");
+console.log(mNAME);
+
